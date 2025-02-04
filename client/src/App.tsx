48979525498c8be76175ddb54000bd24fd64eb18
DhishonThomas <dhishonthomas23@gmail.com>
2025-02-04 20:00:57 +0530

    Updated image upload interface in the client side , ...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,111 +1,181 @@
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 
-
 interface ImagePreview {
   frontPrev: string;
   backPrev: string;
 }
 
 interface Data {
-  frontImage: File|null;
-  backImage:File|null
+  frontImage: File | null;
+  backImage: File | null;
 }
 
 const App = () => {
+  const [data, setData] = useState<Data | null>({
+    frontImage: null,
+    backImage: null,
+  });
+  let frontImage = useRef<any>(null);
+  let backImage = useRef<any>(null);
+  let [submitButton, setSubmitButton] = useState(true);
 
-const [data,setData]=useState<Data|null>({
-  frontImage:null,
-  backImage:null
-})
-  let frontImage=useRef<any>(null)
-let backImage=useRef<any>(null)
-let [submitButton,setSubmitButton]=useState(true)
+  async function sendImages() {
+    if (data?.backImage && data?.frontImage) {
+      const formData = new FormData();
 
-async function sendImages() {
-  
-if(data?.backImage&&data?.frontImage){
-  const formData=new FormData()
-
-  formData.append("frontImage",data?.frontImage)
-  formData.append("backImage",data?.backImage)
-  const response=await axios.post("http://localhost:2000/api/upload-images",formData,{
-    headers:{
-      'Content-Type': "multipart/form-data",
+      formData.append("frontImage", data?.frontImage);
+      formData.append("backImage", data?.backImage);
+      const response = await axios.post(
+        "http://localhost:2000/api/upload-images",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      if (response.data.status) {
+        alert("data reached server succssfully.");
+      }
+      console.log("responce", response.data);
     }
-  })
-  if(response.data.status){
-    alert("data reached server succssfully.")
+
+    // setData(response.data)
   }
-  console.log("responce",response.data)
-  
-}
 
+  const [imagePrev, setImagePrev] = useState<ImagePreview>({
+    frontPrev: "",
+    backPrev: "",
+  });
+  useEffect(() => {
+    if (data?.backImage && data.frontImage) {
+      setSubmitButton(false);
+    } else {
+      setSubmitButton(true);
+    }
+  }, [data]);
 
-  // setData(response.data)
-}
-
-const [imagePrev,setImagePrev]=useState<ImagePreview>({
-  frontPrev:"",
-  backPrev:""
-})
-useEffect(()=>{
-  if(data?.backImage&&data.frontImage){
-    setSubmitButton(false)
-  }else{
-    setSubmitButton(true)
-  }
-},[data])
-
-const deleteFront=()=>{
-  frontImage.current.value=""
-  URL.revokeObjectURL(imagePrev.frontPrev)
-  setData((perv:any)=>({...perv,frontImage:null}))
-  setImagePrev((perv)=>({...perv,frontPrev:""}))
-}
-const deleteBack=()=>{
-  backImage.current.value=""
-  URL.revokeObjectURL(imagePrev.backPrev)
-  setData((perv:any)=>({...perv,backImage:null}))
-  setImagePrev((perv)=>({...perv,backPrev:""}))
-}
-const updateFront=async()=>{
-  if(imagePrev.frontPrev){
-    URL.revokeObjectURL(imagePrev.frontPrev)
-  }
-    setData((prev:any) => ({...prev,frontImage:frontImage.current.files[0]}));
+  const deleteFront = () => {
+    frontImage.current.value = "";
+    URL.revokeObjectURL(imagePrev.frontPrev);
+    setData((perv: any) => ({ ...perv, frontImage: null }));
+    setImagePrev((perv) => ({ ...perv, frontPrev: "" }));
+  };
+  const deleteBack = () => {
+    backImage.current.value = "";
+    URL.revokeObjectURL(imagePrev.backPrev);
+    setData((perv: any) => ({ ...perv, backImage: null }));
+    setImagePrev((perv) => ({ ...perv, backPrev: "" }));
+  };
+  const updateFront = async () => {
+    if (imagePrev.frontPrev) {
+      URL.revokeObjectURL(imagePrev.frontPrev);
+    }
+    setData((prev: any) => ({
+      ...prev,
+      frontImage: frontImage.current.files[0],
+    }));
     const prevImage = URL.createObjectURL(frontImage.current.files[0]);
-    setImagePrev((prev)=>({...prev,frontPrev:prevImage}));
+    setImagePrev((prev) => ({ ...prev, frontPrev: prevImage }));
     console.log(prevImage);
-}
-const updateBack=async()=>{
-  if(imagePrev.backPrev){
-    URL.revokeObjectURL(imagePrev.backPrev)
-  }
-  setData((prev:any)=>({...prev,backImage:backImage.current.files[0]}))
-  const prevImage=URL.createObjectURL(backImage.current.files[0])
-  setImagePrev((prev)=>({...prev,backPrev:prevImage}))
-}
+  };
+  const updateBack = async () => {
+    if (imagePrev.backPrev) {
+      URL.revokeObjectURL(imagePrev.backPrev);
+    }
+    setData((prev: any) => ({
+      ...prev,
+      backImage: backImage.current.files[0],
+    }));
+    const prevImage = URL.createObjectURL(backImage.current.files[0]);
+    setImagePrev((prev) => ({ ...prev, backPrev: prevImage }));
+  };
 
   return (
-    <div className="text-black-300 font-bold text-2xl bg-white min-h-screen">
-      This is for ocr-aadhaar-system.
-    
-<input type="file" ref={frontImage}  onChange={updateFront}/>
-<button className="border bg-red-300" onClick={deleteFront}>delete front</button>
-  {
-imagePrev.frontPrev&&  <img src={imagePrev.frontPrev} alt="Uploaded preview" style={{ maxWidth: '100%', height: 'auto' }} />
+    <div className="min-h-screen bg-gray-100 flex">
+      <div className="w-1/2 p-6 bg-white border-r">
+        <div className="bg-white rounded-lg p-4">
+          <p className="text-gray-700 font-medium mb-2">Front Image</p>
+          <input
+            type="file"
+            name="front-image"
+            ref={frontImage}
+            onChange={updateFront}
+            accept=".png, .jpg, .jpeg"
+            maxLength={1}
+            className="w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-emerald-50 file:text-emerald-700 hover:file:bg-emerald-100"
+          />
+          <div className="mt-4 w-full h-48 border-2 border-dashed border-gray-400 rounded-lg flex items-center justify-center bg-gray-50">
+            {imagePrev.frontPrev ? (
+              <img
+                src={imagePrev.frontPrev}
+                alt="Front Preview"
+                className="max-w-full max-h-full object-contain rounded-lg"
+              />
+            ) : (
+              <p className="text-gray-400">
+                Front image preview will appear here
+              </p>
+            )}
+          </div>
+          {imagePrev.frontPrev && (
+            <button
+              className="border bg-red-500 rounded-md p-1 m-2 cursor-pointer"
+              onClick={deleteFront}
+            >
+              delete front
+            </button>
+          )}
+        </div>
+        <div className="bg-white rounded-lg p-4">
+          <p className="text-gray-700 font-medium mb-2">Back Image</p>
+          <input
+            type="file"
+            name="back-image"
+            onChange={updateBack}
+            ref={backImage}
+            accept=".png, .jpg, .jpeg"
+            maxLength={1}
+            className="w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-emerald-50 file:text-emerald-700 hover:file:bg-emerald-100"
+          />
+          <div className="mt-4 w-full h-48 border-2 border-dashed border-gray-400 rounded-lg flex items-center justify-center bg-gray-50">
+            {imagePrev.backPrev ? (
+              <img
+                src={imagePrev.backPrev}
+                alt="Back Preview"
+                className="max-w-full max-h-full object-contain rounded-lg"
+              />
+            ) : (
+              <p className="text-gray-400">
+                Back image preview will appear here
+              </p>
+            )}
+          </div>
+          {imagePrev.backPrev && (
+            <button
+              className="border bg-red-500 rounded-md p-1 m-2 "
+              onClick={deleteBack}
+            >
+              delete back
+            </button>
+          )}
+        </div>
 
-  }
-    {
-imagePrev.backPrev&&  <img src={imagePrev.backPrev} alt="Uploaded preview" style={{ maxWidth: '100%', height: 'auto' }} />
+        <div className="flex justify-center font-bold">
+          <button
+            className={`bg-blue-500 p-2 w-3xl rounded-md ${
+              submitButton ? "cursor-not-allowed" : "cursor-pointer"
+            }`}
+            disabled={submitButton}
+            onClick={sendImages}
+          >
+            submit
+          </button>
+        </div>
+      </div>
 
-  }
-  
-<input type="file" onChange={updateBack} ref={backImage}/>
-<button className="border bg-red-300 " onClick={deleteBack}>delete back</button>
-
-<button disabled={submitButton} onClick={sendImages}>click me</button>
+      <div className="w-1/2 p-6">hai</div>
     </div>
   );
 };

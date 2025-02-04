fdaa3ccdd6e492ae86762665bd86764fc61c5b71
DhishonThomas <dhishonthomas23@gmail.com>
2025-02-04 11:34:25 +0530

    Client Added

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-black-300 font-bold text-2xl bg-white min-h-screen">
+      This is for ocr-aadhaar-system.
+    </div>
+  );
+};
+
+export default App;

20237a767fc3cbce26d88b5502273838488e63d9
DhishonThomas <dhishonthomas23@gmail.com>
2025-02-04 11:47:44 +0530

    backed cckend code refactored...

--- server/server.js
+++ server/server.js
@@ -1,10 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import { createWorker } from 'tesseract.js';
-import fs from 'fs'
-import multer from 'multer';
-import path from 'path';
-import { Router } from 'express';
 import router from './app/routes/uploadImages.js'
 const app=express()
 app.use(cors({ 
@@ -12,42 +7,6 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
-
-
-
-// app.post("/api",upload.fields([{name:"frontImage"},{name:"backImage"}]),async(req,res)=>{
-// console.log(req.files)
-
-// while (stack.length>0) {
-//     let filePath=stack.pop()
-//     fs.readFile('./uploads/'+filePath,(err,data)=>{
-//         if(err){
-//             console.log(err)
-//         }else{
-//             (
-//                 async ()=>{
-//                     const worker=await createWorker('eng')
-//                     const ret=await worker.recognize(data)
-//                     console.log(ret.data.text)
-
-//                     await worker.terminate()
-
-//                 }
-//             )()
-
-//             fs.unlink('./uploads/'+filePath,(err)=>{
-//                 if(err){
-//                     console.log(err)
-//                 }else{
-//                     console.log("File deleted succesfully")
-//                 }
-//             })
-//         }
-//     })
-// }
-
-// res.json({status:true,data:"data reached server"})
-// })
 app.use("/api",router)
 app.listen(2000,()=>{
 console.log("port is running on ",2000)

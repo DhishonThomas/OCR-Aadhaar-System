46c5517b190b5ab61809571c97b9354ac1ed5ff5
DhishonThomas <dhishonthomas23@gmail.com>
2025-02-04 11:42:53 +0530

    Added server image stack , and reading , useing tesract.js... also text is geting....

--- server/server.js
+++ server/server.js
@@ -1,48 +1,51 @@
 import express from 'express'
 import cors from 'cors'
-import Tesseract from 'tesseract.js';
 import { createWorker } from 'tesseract.js';
 import fs from 'fs'
 import multer from 'multer';
 import path from 'path';
 
 const app=express()
-const imagePath="./app/adhar.jpeg"
+
 app.use(cors({ 
     origin: ['http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
-
-
+let fileName=""
+let stack=[]
 const storage=multer.diskStorage({
     destination:"./uploads",
     filename:(req,file,cb)=>{
-        cb(null,file.fieldname+"-"+Date.now()+path.extname(file.originalname))
+fileName=file.fieldname+"-"+Date.now()+path.extname(file.originalname)
+stack.push(fileName)
+console.log(fileName)
+        cb(null,fileName)
     }
 })
-
 const upload=multer({storage})
-
 app.post("/",upload.fields([{name:"frontImage"},{name:"backImage"}]),async(req,res)=>{
-
 console.log(req.files)
+while (stack.length>0) {
+    fs.readFile('./uploads/'+stack.pop(),(err,data)=>{
+        if(err){
+            console.log(err)
+        }else{
+            (
+                async ()=>{
+                    const worker=await createWorker('eng')
+                    const ret=await worker.recognize(data)
+                    console.log(ret.data.text)
+                    
+                    await worker.terminate()
+
+                }
+            )()
+        }
+    })
+}
+
 res.json({status:true,data:"data reached server"})
-//     fs.readFile(imagePath,(err,data)=>{
-
-//     if(err){
-//         console.error(err)
-//     }else{
-//         (async () => {
-//             const worker = await createWorker('eng');
-//             const ret = await worker.recognize(data);
-//             console.log(ret.data.text);
-//             await worker.terminate();
-//           })();
-
-//     }
-// })
-    
 })
 
 app.listen(2000,()=>{
